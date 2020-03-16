026fbc845af55f337aaf69f818b46bd1e434bcc7
geysafe <62259318+geysafe@users.noreply.github.com>
2020-03-16 15:20:11 -0300

    Add files via upload
    aprendendo a usar ESTRUTURAS ANINHADAS
    inclusão da tarifa promocional $0,08 acima de 800 minutos

--- Telefonia Ttchau.py
+++ Telefonia Ttchau.py
@@ -2,9 +2,18 @@ minutos = float(input("Qual foi o consume em minutos: \n"))
 
 if minutos < 200:
     conta = minutos * 0.20
-elif minutos >= 200 and minutos <= 400:
-    conta = minutos * 0.18
 else:
-    conta = minutos * 0.15
+    if minutos <= 400:
+        conta = minutos * 0.18
+    else:
+        if minutos <= 800:
+            conta = minutos * 0.15
+        else:
+            conta = minutos * 0.08
 
 print("Sua conta é de R$", conta)
+
+''' 
+aprendendo a usar ESTRUTURAS ANINHADAS
+inclusão da tarifa promocional $0,08 acima de 800 minutos
+'''
